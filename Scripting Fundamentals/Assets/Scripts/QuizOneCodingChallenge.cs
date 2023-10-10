4aff406481acbaa97d728f8761e0acaa1ad8a2b1
jrs <hello@jrs.dev>
2023-10-10 11:59:48 -0700

    updating files

---
+++ Scripting Fundamentals/Assets/Scripts/QuizOneCodingChallenge.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizOneCodingChallenge : MonoBehaviour
+{
+    public int Score = 0;
+    public string PlayerName = "Justin";
+    public int Health = 100;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        IncreseScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void IncreseScore()
+    {
+        Score = Score + 10;
+    }
+
+    private void PrintPlayerName()
+    {
+        Debug.Log(PlayerName);
+    }
+
+    private void DecreaseHealth()
+    {
+        Health = Health - 20;
+    }
+}
